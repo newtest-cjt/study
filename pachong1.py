82319c11d323b3c472d774818fe6db5595193cf7
newtest-cjt <953400851@qq.com>
2018-04-13 16:32:02 +0800

    Update pachong1.py

--- pachong1.py
+++ pachong1.py
@@ -6,26 +6,75 @@ import xlrd
 import xlwt
 import win32ui
 import time
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities 
+from selenium.webdriver.common.action_chains import ActionChains
+num=[]
 loop=1
 Key=[]
+def get_cookie_from_network(): 
+    url_login = 'http://www.qichacha.com/user_login' 
+    driver = webdriver.Chrome()
+    driver.get(url_login) 
+    driver.set_window_size(1366, 768)
+    time.sleep(1)a
+    cc1=driver.find_element_by_link_text('密码登录')
+    cc1.click()
+    xpath1=driver.find_element_by_xpath('//*[@id="nameNormal"]')
+    xpath1.send_keys('15313698527 ') 
+    xpath2=driver.find_element_by_xpath('//*[@id="pwdNormal"]')
+    xpath2.send_keys('only3.14')
+    source=driver.find_element_by_xpath("//*[@id='nc_2__scale_text']/span") 
+    ActionChains(driver).drag_and_drop_by_offset(source,400,0).perform()
+    time.sleep(1)
+    cc2=driver.find_element_by_xpath('//*[@id="user_login_normal"]/button')
+    cc2.click()
+    driver.save_screenshot('2.png')
+    time.sleep(0.5)
+    a = driver.get_cookies() 
+    driver.quit()
+    for i0 in range(len(a)):
+        if a[i0]['name']=='PHPSESSID':
+            num.append(i0)
+    for i1 in range(len(a)):
+        if a[i1]['name']=='UM_distinctid':
+            num.append(i1)
+    for i2 in range(len(a)):
+        if a[i2]['name']=='zg_did':
+            num.append(i2)
+    for i3 in range(len(a)):
+        if a[i3]['name']=='acw_tc':
+            num.append(i3)
+    for i4 in range(len(a)):
+        if a[i4]['name']=='_uab_collina':
+            num.append(i4)
+    for i5 in range(len(a)):
+        if a[i5]['name']=='Hm_lvt_3456bee468c83cc63fb5147f119f1075':
+            num.append(i5)
+    for i6 in range(len(a)):
+        if a[i6]['name']=='_umdata':
+            num.append(i6)
+    for i7 in range(len(a)):
+        if a[i7]['name']=='CNZZDATA1254842228':
+            num.append(i7)
+    for i8 in range(len(a)):
+        if a[i8]['name']=='zg_de1d1a35bfa24ce29bbf2c7eb17e6c4f':
+            num.append(i8)
+    for i9 in range(len(a)):
+        if a[i9]['name']=='Hm_lpvt_3456bee468c83cc63fb5147f119f1075':
+            num.append(i9)
+
+    ress=a[num[0]]['name']+'='+a[num[0]]['value']+'; '+a[num[1]]['name']+'='+a[num[1]]['value']+'; '+a[num[2]]['name']+'='+a[num[2]]['value']+'; '+a[num[3]]['name']+'='+a[num[3]]['value']+'; '+a[num[4]]['name']+'='+a[num[4]]['value']+'; '+a[num[5]]['name']+'='+a[num[5]]['value']+';'+' hasShow=1; '+a[num[6]]['name']+'='+a[num[6]]['value']+'; '+a[num[7]]['name']+'='+a[num[7]]['value']+'; '+a[num[8]]['name']+'='+a[num[8]]['value']+'; '+a[num[9]]['name']+'='+a[num[9]]['value']
+    driver.quit()
+    return ress
+
 def chfile():
     global filename
     dlg = win32ui.CreateFileDialog(1) # 1表示打开文件对话框 
     dlg.DoModal()
     filename=dlg.GetPathName() # 获取选择的文件名称
     return filename
-headers={
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Cache-Control': 'max-age=0',
-    'Connection': 'keep-alive',
-    'Cookie': 'PHPSESSID=tqjeo50nql148lrutujhfhaho6; UM_distinctid=162b7e5aabe228-04390d9510467d-4446062d-100200-162b7e5aabf187; zg_did=%7B%22did%22%3A%20%22162b7e5aadae6-0098947d54de89-4446062d-100200-162b7e5aadb170%22%7D; hasShow=1; acw_tc=AQAAAFQ2wn+vVAUATKmHPXGYj9y6lQbU; _uab_collina=152350371706055027040967; _umdata=486B7B12C6AA95F255DDA31F1200699BA60BC480571BFDF59869EFC1C97508EDB3D8A2BA4D66C954CD43AD3E795C914C4A16773D94CAE3E427FB3595A64627B0; Hm_lvt_3456bee468c83cc63fb5147f119f1075=1523503705,1523505712; CNZZDATA1254842228=83472213-1523502333-https%253A%252F%252Fwww.baidu.com%252F%7C1523523933; acw_sc__=5acf276dd874725127c6b693c48ad9e6b40c32a4; zg_de1d1a35bfa24ce29bbf2c7eb17e6c4f=%7B%22sid%22%3A%201523522831823%2C%22updated%22%3A%201523525497329%2C%22info%22%3A%201523503704801%2C%22superProperty%22%3A%20%22%7B%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22%22%2C%22cuid%22%3A%20%222f265445e47660bc71fb5a438c4dd443%22%7D; Hm_lpvt_3456bee468c83cc63fb5147f119f1075=1523525497',    
-    'Host': 'www.qichacha.com',
-    'Referer': 'http://www.qichacha.com/',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'         
-       }
+
 filename=chfile()#选文件
 data=xlrd.open_workbook(filename)
 sheet_name=data.sheet_names() #获取所有sheet的名字
@@ -47,7 +96,20 @@ a=getgongsiname()
 b=getname()
 c=getphonenumber()
 ############################################################################################################
-
+cookies=get_cookie_from_network()
+headers={
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie':cookies,
+    'Host': 'www.qichacha.com',
+    'Referer': 'http://www.qichacha.com/user_login',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'         
+       }
+print(headers)
 newfile=xlwt.Workbook()
 sheet1 = newfile.add_sheet('企查猫(企业查询宝)',cell_overwrite_ok=True)
 sheet1.write(0,0,sheet.col_values(a)[0])
@@ -64,6 +126,24 @@ for key in Key:
     res=requests.get(url=url,headers=headers).content.decode('utf-8')
     z=etree.HTML(res)
     l=z.xpath('//*[@id="countOld"]/span')
+    if len(l)==0:
+        print('change cookie')
+        cookies=get_cookie_from_network()
+        headers={
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Cookie':cookies,
+            'Host': 'www.qichacha.com',
+            'Referer': 'http://www.qichacha.com/user_login',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'         
+               }
+        res=requests.get(url=url,headers=headers).content.decode('utf-8')
+        z=etree.HTML(res)
+        l=z.xpath('//*[@id="countOld"]/span')
     for i in l:
         value=int(i.text)
         if value>0 and value <3:
