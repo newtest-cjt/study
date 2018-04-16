fd804bb3b6a04b48eaa4319050a79f0734f0cb45
newtest-cjt <953400851@qq.com>
2018-04-16 19:16:04 +0800

    Update pachong1.py

--- pachong1.py
+++ pachong1.py
@@ -12,9 +12,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 num=[]
 loop=1
 Key=[]
+driver = webdriver.PhantomJS()
 def get_cookie_from_network(): 
     url_login = 'http://www.qichacha.com/user_login' 
-    driver = webdriver.Chrome()
     driver.get(url_login) 
     driver.set_window_size(1366, 768)
     time.sleep(1)
@@ -22,16 +22,26 @@ def get_cookie_from_network():
     cc1.click()
     xpath1=driver.find_element_by_xpath('//*[@id="nameNormal"]')
     xpath1.send_keys('15313698527 ') 
+    time.sleep(2)
     xpath2=driver.find_element_by_xpath('//*[@id="pwdNormal"]')
     xpath2.send_keys('only3.14')
+    time.sleep(2)
     source=driver.find_element_by_xpath("//*[@id='nc_2__scale_text']/span") 
-    ActionChains(driver).drag_and_drop_by_offset(source,400,0).perform()
-    time.sleep(1)
+    action = ActionChains(driver)
+    def huadong(pra):
+            action.click_and_hold(pra).perform() #鼠标左键按下不放
+            action.move_by_offset(400, 0).perform() #平行移动鼠标
+            time.sleep(2) #等待停顿时间
+            
+    try:
+        huadong(source)
+    except :
+        huadong(source)
     cc2=driver.find_element_by_xpath('//*[@id="user_login_normal"]/button')
     cc2.click()
-    driver.save_screenshot('2.png')
-    time.sleep(0.5)
+    time.sleep(1)
     a = driver.get_cookies() 
+    driver.save_screenshot('2.png')
     driver.quit()
     for i0 in range(len(a)):
         if a[i0]['name']=='PHPSESSID':
@@ -109,7 +119,7 @@ headers={
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'         
        }
-print(headers)
+
 newfile=xlwt.Workbook()
 sheet1 = newfile.add_sheet('企查猫(企业查询宝)',cell_overwrite_ok=True)
 sheet1.write(0,0,sheet.col_values(a)[0])
@@ -127,23 +137,36 @@ for key in Key:
     z=etree.HTML(res)
     l=z.xpath('//*[@id="countOld"]/span')
     if len(l)==0:
-        print('change cookie')
-        cookies=get_cookie_from_network()
-        headers={
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Cookie':cookies,
-            'Host': 'www.qichacha.com',
-            'Referer': 'http://www.qichacha.com/user_login',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'         
-               }
-        res=requests.get(url=url,headers=headers).content.decode('utf-8')
-        z=etree.HTML(res)
-        l=z.xpath('//*[@id="countOld"]/span')
+        source1=driver.find_element_by_xpath("//*[@id='nc_1__scale_text']/span") 
+        action1 = ActionChains(driver)
+        def huadong():
+            action1.click_and_hold(source1).perform() #鼠标左键按下不放
+            action1.move_by_offset(400, 0).perform() #平行移动鼠标
+            time.sleep(2) #等待停顿时间
+            driver.save_screenshot('3.png')
+            cc3=driver.find_element_by_xpath("//*[@id='verify']/strong")
+            cc3.click()
+            print('验证滑块成功')
+            time.sleep(1)
+        huadong(source1)
+        if len(l)==0:
+            print('change cookie')
+            cookies=get_cookie_from_network()
+            headers={
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cache-Control': 'max-age=0',
+                'Connection': 'keep-alive',
+                'Cookie':cookies,
+                'Host': 'www.qichacha.com',
+                'Referer': 'http://www.qichacha.com/user_login',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'         
+                   }
+            res=requests.get(url=url,headers=headers).content.decode('utf-8')
+            z=etree.HTML(res)
+            l=z.xpath('//*[@id="countOld"]/span')
     for i in l:
         value=int(i.text)
         if value>0 and value <3:
@@ -154,4 +177,5 @@ for key in Key:
         else:
             print(str(numlist[loop])+':'+str(value)+'扔掉')
         loop=loop+1 
+    time.sleep(2)
     newfile.save('D:\\test.xlsx')
